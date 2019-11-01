e44a99c50800036b028a7b8c684c0bbd32c0a90e
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-01 14:10:19 +0100

    Add middleware files

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 var express = require('express');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-
 var app = express();
-var stringifyFile;
 
-app.use(bodyParser.json());
+app.use('/store', (req, res, next) => {
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+})
 
-
-app.get('/getNote', (req, res) => {
-    fs.readFile('./test.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        stringifyFile = data
-        res.send(data);
-    });
+app.get('/', (req, res) => {
+    res.send("Hello world"); 
 });
 
-app.post('/updateNote/:note', (req, res) => {
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-        if (err) throw err;
-        res.send(stringifyFile + req.params.note);
-        });        
-});
+app.get('/store', (req, res) => {
+    res.send("To jest sklep");
+})
 
 app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
+
